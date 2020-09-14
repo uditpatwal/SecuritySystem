dd0cecf9d4d31203d8a2cf839c8187608f91ba7f
Udit <uditpatwal@gmail.com>
2020-09-13 15:52:53 -0900

    user authentication with forms and flashes messages

--- Website.py
+++ Website.py
@@ -5,8 +5,20 @@ import imutils
 import time
 import cv2
 import flask_login
+from config import Config
 from imutils.video import VideoStream
-from flask import Response, Flask, render_template, redirect, url_for, request
+from flask import Response, Flask, render_template, redirect, url_for, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
 class Website:
     
@@ -19,7 +31,7 @@ class Website:
 
         # Initialize our Flaskapp
         self.app = Flask(__name__)
-        self.app.secret_key = 'my key is super duper uber ultra mega secret' # Please change this key!!!!!!
+        self.app.config.from_object(Config)
         # Initialize login manager for individual users
         self.login_manager = flask_login.LoginManager()
         self.login_manager.init_app(self.app)
@@ -43,7 +55,7 @@ class Website:
 
 
         @self.login_manager.request_loader
-        def request_laoder(request):
+        def request_loader(request):
             email = request.form.get('email')
             if email not in self.users:
                 return
@@ -54,6 +66,12 @@ class Website:
             user.is_authenticated = request.form['password'] == self.users[email]['password']
         
             return user
+        
+        @self.login_manager.unauthorized_handler
+        def unauthorized_handler():
+            flash('You cannot access Videos until logged in')
+            return redirect(url_for('home'))
+
 
         def generate():
             while True:
@@ -93,30 +111,30 @@ class Website:
         # Route for handling the login page logic
         @self.app.route('/login', methods=['GET', 'POST'])
         def login():
-            if request.method == 'GET':
-                return '''
-                       <form action='login' method='POST'>
-                        <input type='text' name='email' id='email' placeholder='email'/>
-                        <input type='password' name='password' id='password' placeholder='password'/>
-                        <input type='submit' name='submit'/>
-                       </form>
-                       '''
-
-            email = request.form['email']
-            if request.form['password'] == self.users[email]['password']:
-                user = User()
-                user.id = email
-                flask_login.login_user(user)
-                return redirect(url_for('home'))
-
-            return 'Bad login'
+            form = LoginForm()
+            if form.validate_on_submit():
+                flash('Login requested for user {}, remember_me={}'.format(
+                    form.username.data, form.remember_me.data))
+                if form.username.data not in self.users:
+                    flash('Invalid username or password')
+                    return redirect(url_for('login'))
+                elif form.password.data == self.users[form.username.data]['password']:
+                    user = User()
+                    user.id = form.username.data
+                    flask_login.login_user(user)
+                    return redirect('/')
+                else:
+                    flash('Invalid username or password')
+                    return redirect(url_for('login'))
+            return render_template('login.html', title = 'Sign In', form = form)
+            
 
         
         @self.app.route("/pepperoni")
         def pepperoni():
             return "Hello! Here are some nice pepperonis!"
 
-        @self.app.route("/video/")
+        @self.app.route("/video")
         @flask_login.login_required
         def video():
             return render_template("video.html")
